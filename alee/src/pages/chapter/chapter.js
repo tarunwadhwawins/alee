5731f4cf1995456881e3a10fdd85aa81e8af20af
Lovenish Kumar <lovenish.kumar@ditstek.com>
2021-09-02 10:01:48 +0530

    changes in  profile

--- alee/src/pages/chapter/chapter.js
+++ alee/src/pages/chapter/chapter.js
@@ -28,7 +28,7 @@ function ChapterPage() {
 	const openModal2 = () => {
 		setSubtitle(!subtitle)}
 
-	const openModal3 = (chapterData) => {
+	const openModal3 = (props) => {
 		setSummary(!summary)
 		const data = JSON.parse(props)
 		setSummaryData(data)
